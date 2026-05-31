f04f23650633541a269d8321cf2666f9c111bbbd
msolerd <mariosolerdoria@gmail.com>
2026-05-31 13:37:46 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -5,10 +5,10 @@
 
 const TOOLTIPS = {
   intensidad: 'Es la fuerza del sabor. 1 grano significa que es suave.<br>5 que es fuerte, oscuro y persistente.',
-  acidez:     'Es el tipo de sabor.1 grano significa que es clásico y equilibrado.<br>5 que es exótico fresco y afrutado.',
-  cuerpo:     'Es la densidad en boca.1 significa que es muy ligero.<br>5 granos que es denso y cremoso.',
-  dulzura:    'Es la suavidad natural.1 grano significa que es amargo.<br>5 granos que es suave con notas a caramelo.',
-  aroma:      'Es el olor del café.1/2 granos significa aroma clásico.<br>2/3 granos toque a frutos secos; 4/5 granos toque frutal.',
+  acidez:     'Es el tipo de sabor. 1 grano significa que es clásico y equilibrado.<br>5 que es exótico fresco y afrutado.',
+  cuerpo:     'Es la densidad en boca. 1 significa que es muy ligero.<br>5 granos que es denso y cremoso.',
+  dulzura:    'Es la suavidad natural. 1 grano significa que es amargo.<br>5 granos que es suave con notas a caramelo.',
+  aroma:      'Es el olor del café. 1/2 granos significa aroma clásico.<br>2/3 granos toque a frutos secos; 4/5 granos toque frutal.',
   cafeina:    'Grano lleno = con cafeína<br>Grano vacío = descafeinado'
 };
 
